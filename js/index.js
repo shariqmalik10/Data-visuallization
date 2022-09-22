549714c007882f6c0e1d816017386918226514b0
Shariq Nadeem Malik <shariqnadeem21@hotmail.com>
2022-09-22 17:07:49 +0800

    html
    index html

---
+++ js/index.js
@@ -0,0 +1,5 @@
+var spec2 = "visualization.vg.json";
+vegaEmbed('#map_plot', spec2).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+        
